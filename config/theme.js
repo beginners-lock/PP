287fabf32af4b20ac86d417bb89673ecd98b3a67
beginners-lock <chukwurophi@gmail.com>
2023-09-18 09:23:33 +0100

    assets

---
+++ config/theme.js
@@ -0,0 +1,41 @@
+export const colors = {
+    light: {
+        background: 'white',
+        btn1: 'black',
+        btn2: '#928E8E',
+        text1: 'black',
+        text2: '#646060',
+        text3: '#4285F4',
+        text4: 'black',
+        text5: '#646863',
+        inputlabel1: '#646060',
+        inputborder1: '#928E8E'
+    },
+
+    dark: {
+        background: '#181818',
+        btn1: '#1E9E40',
+        btn2: '#1E9E40',
+        text1: 'white',
+        text2: '#C8D1DB',
+        text3: '#1E9E40',
+        text4: '#949494',
+        text5: 'white',
+        inputlabel1: 'white',
+        inputborder1: 'white'
+    },
+
+    lightTab: {
+        backgroundColor:'#F8F8F8',
+        borderColor:'#C8D1DB',
+    },
+
+    darkTab: {
+        backgroundColor:'#343434',
+        borderColor:'#343434',
+    }
+
+    
+
+
+}
